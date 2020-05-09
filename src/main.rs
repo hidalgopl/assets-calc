b0e67fc15fa7e99158bdd7120a8a966aeb08e4f9
hidalgopl <pawebojanowski@gmail.com>
2020-05-09 12:07:12 +0200

    use smol async runtime, format report nicely

--- src/main.rs
+++ src/main.rs
@@ -5,106 +5,146 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
-struct RespError<'a> {
-	code: i32,
-	msg: &'a str,
-	info: &'a str,
+#[derive(Deserialize, Debug)]
+struct Config {
+    fiat_url: String,
+    crypto_url: String,
 }
 
-struct MetalResp {
-	gold_ounce: f64,
-	silver_ounce: f64,
+#[derive(Deserialize, Debug)]
+struct RespError {
+    code: i32,
+    msg: String,
+    info: String,
 }
 
+#[derive(Debug, Deserialize)]
 struct Cryptos {
-	ADA: f64,
-	BTC: f64,
-	ETH: f64,
-	XMR: f64,
+    #[serde(rename = "ADA")]
+    ada: f64,
+    #[serde(rename = "BTC")]
+    btc: f64,
+    #[serde(rename = "ETH")]
+    eth: f64,
+    #[serde(rename = "XMR")]
+    xmr: f64,
 }
 
-struct CryptoResp<'a> {
-	success: &'a bool,
-	rates: Cryptos,
-	error: RespError<'a>,
+#[derive(Debug, Deserialize)]
+struct CryptoResp {
+    success: bool,
+    rates: Cryptos,
 }
 
+#[derive(Debug, Deserialize)]
 struct FiatResp {
-	rates: Fiats,
+    rates: Fiats,
 }
 
+#[derive(Debug, Deserialize)]
 struct Fiats {
-	AUD: f64,
-	CAD: f64,
+    #[serde(rename = "AUD")]
+    aud: f64,
+    #[serde(rename = "CAD")]
+    cad: f64,
 }
 
 #[derive(Deserialize, Debug)]
 struct MyAssets {
-	XMR: f64,
-	ADA: f64,
-	ETH: f64,
-	BTC: f64,
-	crypto_pln: f64,
-	metal_pln: f64,
-	gold_ounces: f64,
-	silver_ounces: f64,
-	fiat_pln: f64,
-	AUD: f64,
-	CAD: f64,
+    #[serde(rename = "XMR")]
+    xmr: f64,
+    #[serde(rename = "ADA")]
+    ada: f64,
+    #[serde(rename = "ETH")]
+    eth: f64,
+    #[serde(rename = "BTC")]
+    btc: f64,
+    crypto_pln: f64,
+    metal_pln: f64,
+    fiat_pln: f64,
+    #[serde(rename = "AUD")]
+    aud: f64,
+    #[serde(rename = "CAD")]
+    cad: f64,
 }
 
-fn read_assets_from_file<P: AsRef<Path>>(path: P) -> Result<MyAssets, Box<Error>> {
-	let file = File::open(path)?;
-	let reader = BufReader::new(file);
-	let m = serde_json::from_reader(reader)?;
-	Ok(m)
+struct FinancialReport {
+    crypto_invested: f64,
+    fiat_invested: f64,
+    crypto_current: f64,
+    fiat_current: f64,
 }
 
-fn calc_metal_value(amount: &MyAssets, rates: MetalResp) -> f64 {
-	amount.gold_ounces * rates.gold_ounce + amount.silver_ounces * rates.silver_ounce
+impl std::fmt::Display for FinancialReport {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        let dt = chrono::Utc::now();
+        let total_invested = self.crypto_invested + self.fiat_invested;
+        let current = self.crypto_current + self.fiat_current;
+        write!(f, "------------------------------------------------\n\
+        | -------------- Financial Report ------------ |\n\
+        | ------------ {} ----------- |\n\
+        ------------------------------------------------\n\
+        | ------ Crypto current value: {:.2} ------- |\n\
+        | ------ Crypto invested value: {:.2} ------ |\n\
+        | ---- Crypto percentage value: {:.2} % ------ |\n\
+        ------------------------------------------------\n\
+        | ------- Fiat current value: {:.2} ------- |\n\
+        | ------- Fiat invested value: {:.2} ------ |\n\
+        | ----- Fiat percentage value: {:.2}  % ----- |\n\
+        ------------------------------------------------\n\
+        | ---------- Current value: {:.2} --------- |\n\
+        | ------ Percent of invested: {:.2} % ------- |\n\
+        ------------------------------------------------\n",
+               dt.format("%Y-%m-%d %H:%M:%S").to_string(),
+               self.crypto_current,
+               self.crypto_invested,
+               (self.crypto_current * 100.0) / self.crypto_invested,
+               self.fiat_current,
+               self.fiat_invested,
+               (self.fiat_current * 100.0) / self.fiat_invested,
+               current,
+               (current * 100.0) / total_invested)
+    }
+}
+
+fn read_assets_from_file<P: AsRef<Path>>(path: P) -> Result<MyAssets, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let m = serde_json::from_reader(reader)?;
+    Ok(m)
 }
 
 fn calc_crypto_value(amount: &MyAssets, rates: CryptoResp) -> f64 {
-	amount.XMR*rates.rates.XMR + amount.ETH*rates.rates.ETH + amount.BTC*rates.rates.BTC + amount.ADA*rates.rates.ADA
+    amount.xmr * rates.rates.xmr + amount.eth * rates.rates.eth + amount.btc * rates.rates.btc + amount.ada * rates.rates.ada
 }
 
 fn calc_fiat_value(amount: &MyAssets, rates: FiatResp) -> f64 {
-	amount.CAD/rates.rates.CAD + amount.AUD/rates.rates.AUD
+    amount.cad / rates.rates.cad + amount.aud / rates.rates.aud
 }
 
-fn main() {
-    // Read the file contents into a string, returns `io::Result<usize>`
-    let my_assets = read_assets_from_file("assets.json").unwrap();
-    let fake_metal_resp = MetalResp {
-		gold_ounce: 7775.00,
-		silver_ounce: 100.00,
-	};
-	let fake_crypto_rates = Cryptos {
-		ADA: 5.23,
-		BTC: 3.12,
-		ETH: 5234.1,
-		XMR: 12.543,
-	};
-	let fake_crypto_err = RespError {
-		code: 200,
-		msg: "",
-		info: "",
-	};
-	let fake_crypto_resp = CryptoResp {
-		success: &true,
-		rates: fake_crypto_rates,
-		error: fake_crypto_err,
-	};
-	let fake_fiats = Fiats {
-		AUD: 2.99,
-		CAD: 3.11,
-	};
-	let fake_fiat_resp = FiatResp {
-		rates: fake_fiats,
-	};
-	println!("{}", calc_metal_value(&my_assets, fake_metal_resp));
-    //println!("{:#?}", my_assets);
-    println!("{}", calc_crypto_value(&my_assets, fake_crypto_resp));
-	println!("{}", calc_fiat_value(&my_assets, fake_fiat_resp));
-    // `file` goes out of scope, and the "hello.txt" file gets closed
+fn main() -> Result<(), Box<dyn Error>> {
+    smol::run(async {
+        let config = match envy::prefixed("ASSETS_").from_env::<Config>() {
+            Ok(config) => config,
+            Err(error) => panic!("{:#?}", error)
+        };
+        // Read the file contents into a string, returns `io::Result<usize>`
+        let my_assets = read_assets_from_file("assets.json").unwrap();
+        let fiat_body: FiatResp = reqwest::get(&config.fiat_url).await?.json().await?;
+        let fiat_value: f64 = calc_fiat_value(&my_assets, fiat_body);
+        let crypto_body: CryptoResp = reqwest::get(&config.crypto_url).await?.json().await?;
+        let crypto_value: f64 = calc_crypto_value(&my_assets, crypto_body);
+        let fr: FinancialReport = FinancialReport {
+            crypto_current: crypto_value,
+            crypto_invested: my_assets.crypto_pln,
+            fiat_current: fiat_value,
+            fiat_invested: my_assets.fiat_pln,
+        };
+        println!("{}", fr);
+        Ok(())
+    })
 }
